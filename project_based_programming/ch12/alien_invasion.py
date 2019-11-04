8e11b8d6270c416cb37ae4213c6c71199f192fbf
wuxingfeng <771645231@qq.com>
2019-11-04 18:29:19 +0800

    python游戏

--- project_based_programming/ch12/alien_invasion.py
+++ project_based_programming/ch12/alien_invasion.py
@@ -2,6 +2,9 @@ import sys
 import pygame
 
 from project_based_programming.ch12.settings import Settings
+from project_based_programming.ch12.ship import Ship
+import project_based_programming.ch12.game_functions as gf
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -11,16 +14,17 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建一艘飞船
+    ship = Ship(ai_settings, screen)
+
     #开始游戏地主循环
     while True:
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        # 让最近绘制屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        # 每次循环时都重绘屏幕,让最近绘制屏幕可见
+        gf.update_screen(ai_settings, screen, ship)
+
 
 run_game()
 
