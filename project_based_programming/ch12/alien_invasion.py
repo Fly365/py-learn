ef311507f01f0c05f49ed40b72fae2b1290816a8
wuxingfeng <771645231@qq.com>
2019-11-05 17:44:56 +0800

    python游戏

--- project_based_programming/ch12/alien_invasion.py
+++ project_based_programming/ch12/alien_invasion.py
@@ -1,8 +1,11 @@
-import sys
 import pygame
+from pygame.sprite import Group
 
 from project_based_programming.ch12.settings import Settings
 from project_based_programming.ch12.ship import Ship
+from project_based_programming.ch12.game_stats import GameStats
+from project_based_programming.ch12.button import Button
+from project_based_programming.ch12.scoreboard import Scoreboard
 import project_based_programming.ch12.game_functions as gf
 
 
@@ -14,16 +17,36 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
-    ship = Ship(ai_settings, screen)
+    # 创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
 
-    #开始游戏地主循环
+    # 创建一个用于存储游戏统计信息的实例，并创建积分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+    # 创建一个外星人
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 开始游戏地主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
-        ship.update()
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+
+            # 删除已消失的子弹
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets)
         # 每次循环时都重绘屏幕,让最近绘制屏幕可见
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, stats,sb, ship, aliens, bullets, play_button)
 
 
 run_game()
--- project_based_programming/ch12/ship.py
+++ project_based_programming/ch12/ship.py
@@ -1,8 +1,12 @@
 import pygame
+from pygame.sprite import  Sprite
 
-class Ship():
+
+class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
+        # 初始化飞船，并设置其起始位置
+        super(Ship, self).__init__()
         """初始化飞船并设置其初始位置"""
         self.screen = screen
         self.ai_settings = ai_settings
@@ -13,7 +17,7 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        #将没艘新飞船放在屏幕底部中央
+        # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
@@ -23,7 +27,6 @@ class Ship():
         # 移动标志
         self.moving_right = False
         self.moving_left = False
-
 
     def update(self):
         """根据移动标志调整飞船位置"""
@@ -39,3 +42,7 @@ class Ship():
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """让飞船在屏幕居中"""
+        self.center = self.screen_rect.centerx
